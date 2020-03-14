686a161aa36c18d528259ba52e5e88c25171c550
Jude Cooray <ranhiru@gmail.com>
2020-03-14 14:04:03 +1100

    Added tests

--- git_branch_creator.rb
+++ git_branch_creator.rb
@@ -1,6 +1,7 @@
 class GitBranchCreator
   def self.santizied_branch_name(user_input)
     user_input.downcase
+      .gsub(/[^A-Za-z0-9]/, " ")
       .strip
       .squeeze(" ")
       .gsub(" ", "-")
--- spec/git-switcher_spec.rb
+++ spec/git-switcher_spec.rb
@@ -12,7 +12,20 @@ describe "GitBranchCreator" do
     ["Test", "test"],
     ["Test123", "test123"],
     ["Test 123", "test-123"],
-    ["Test 123", "test-123"]
+    ["Test 123", "test-123"],
+    ["BE - Checkout/Consumer portal/Admin portal - Support Amex", "be-checkout-consumer-portal-admin-portal-support-amex"],
+    ["PAYL 12212 FE - Consumer Portal - Pay now/Payment methods/Order details", "payl-12212-fe-consumer-portal-pay-now-payment-methods-order-details"],
+    ["PAYL-9830 - Upgrade Google Maps", "payl-9830-upgrade-google-maps"],
+    ["FE - Mobile Verification - Auto Continue", "fe-mobile-verification-auto-continue"],
+    ["Display App Banner [WAITING ON UK mobile app release]", "display-app-banner-waiting-on-uk-mobile-app-release"],
+    ["API v1: Use order search", "api-v1-use-order-search"],
+    ["Testing X4232 (For Elastic Search)", "testing-x4232-for-elastic-search"],
+    ["Upgrade test #1 __TEST__", "upgrade-test-1-test"],
+    ["Hide \"In-Store\" Link", "hide-in-store-link"],
+    ["Remove obsolete PAYL-2323 work", "remove-obsolete-payl-2323-work"],
+    ["Automate TC#23323 Login", "automate-tc-23323-login"],
+    ["INVALID_TXN_STATE, 'Error'", "invalid-txn-state-error"],
+    ["Run Skipped 20.4 flyway in sandbox/prod (for ANZ and EU)", "run-skipped-20-4-flyway-in-sandbox-prod-for-anz-and-eu"]
   ].each do |expectation|
     it_behaves_like "Sanitized Branch Name", expectation[0], expectation[1]
   end
